08add152927b9fb737a3e9ad2a498d679cfa77cf
ayrtt <ayrttoon@hotmail.com>
2021-03-04 10:46:58 -0300

    Two Dices and Clear button

--- app/src/main/java/com/example/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/example/mydiceroller/MainActivity.kt
@@ -9,23 +9,33 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit  var diceImage : ImageView
+    lateinit  var diceImage01 : ImageView
+    lateinit  var diceImage02 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage01 = findViewById(R.id.dice_image_1)
+        diceImage02 = findViewById(R.id.dice_image_2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clear() }
+
     }
 
     private fun rollDice() {
+        diceImage01.setImageResource(getRandomDiceImage())
+        diceImage02.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
         val randomInt = (1..6).random()
 
-        val drawableResource = when (randomInt) {
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -33,8 +43,11 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+    }
 
-        diceImage.setImageResource(drawableResource)
+    private fun clear() {
+        diceImage01.setImageResource(R.drawable.empty_dice)
+        diceImage02.setImageResource(R.drawable.empty_dice)
     }
 
 }
